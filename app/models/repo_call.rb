b9316709b6e43cb784f5df9f5ba81945f7e236eb
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-02 16:24:03 -0400

    Pass first test for repocall.

--- app/models/repo_call.rb
+++ app/models/repo_call.rb
@@ -5,10 +5,15 @@ class RepoCall
     @repo = get_data
   end
 
+  def repo_name
+    #  @repo.each {|n| n["name"]}
+    @repo.map {|n| n["name"]}
+
+
+  end
+
   def get_data
-
     HTTParty.get("https://api.github.com/users/masonfmatthews/repos?access_token=#{ENV['GITHUB_TOKEN']}")
-
   end
 
 end
--- test/models/repo_call_test.rb
+++ test/models/repo_call_test.rb
@@ -2,20 +2,54 @@ require 'test_helper'
 require 'minitest/pride'
 
 
-# require './app/models/repo_call.rb'
+require './app/models/repo_call.rb'
 
-# class Repocall
-#   def get_data
-#     JSON.parse(File.open("./test/models/repo_call.json").read)
-#   end
-# end
+class Repocall
+  def get_data
+    JSON.parse(File.open("./test/models/repo_call.json").read)
+  end
+end
 
 
 class RepoCallTest < MiniTest::Test
 
-  def test_create_call
-    p RepoCall.new("john")
+  def test_repocall_exists
+    assert RepoCall
   end
 
+  def test_returns_repo_name
+    call = RepoCall.new("masonfmatthews")
+    assert_equal "api_experiments", call.repo_name[0]
+  end
+
+  def test_returns_repo_url
+    call = RepoCall.new("masonfmatthews")
+    assert_equal "https://github.com/masonfmatthews/api_experiments", call.repo_url[0]
+  end
+
+  def test_returns_repo_description
+    call = RepoCall.new("masonfmatthews")
+    assert_equal "Assignments for the Rails course at The Iron Yard's Durham Campus", call.repo_description[8]
+  end
+
+  def test_returns_updated_at
+    call = RepoCall.new("masonfmatthews")
+    assert_equal "2015-05-25T13:07:44Z", call.repo_updated_at[0]
+  end
+
+  def test_returns_language
+    call = RepoCall.new("masonfmatthews")
+    assert_equal "Ruby", repo_language[0]
+  end
+
+  def test_returns_stargazer_count
+    call = RepoCall.new("masonfmatthews")
+    assert_equal 0, call.stargazer_count[0]
+  end
+
+  def test_returns_forks_count
+    call = RepoCall.new("masonfmatthews")
+    assert_equal 0, call.fork_count[0]
+  end
 
 end
