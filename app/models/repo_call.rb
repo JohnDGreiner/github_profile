1285eb1fcd9489c0b270f43ff0245e6ad0f426b7
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-02 15:08:46 -0400

    Add authentication for github.

--- app/models/repo_call.rb
+++ app/models/repo_call.rb
@@ -1,11 +1,13 @@
 class RepoCall
 
-  def intialize(username)
+  def initialize(username)
     @username = username
     @repo = get_data
   end
 
   def get_data
+
+    HTTParty.get("https://api.github.com/users/masonfmatthews/repos?access_token=#{ENV['GITHUB_TOKEN']}")
 
   end
 
--- test/models/repo_call_test.rb
+++ test/models/repo_call_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 require 'minitest/pride'
 
 
-require './app/models/repo_call.rb'
+# require './app/models/repo_call.rb'
 
-class Repocall
-  def get_data
-    JSON.parse(File.open("./test/models/repo_call.json").read)
-  end
-end
+# class Repocall
+#   def get_data
+#     JSON.parse(File.open("./test/models/repo_call.json").read)
+#   end
+# end
 
 
 class RepoCallTest < MiniTest::Test
 
   def test_create_call
-    assert true
+    p RepoCall.new("john")
   end
 
 
