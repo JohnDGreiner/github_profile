a3afaba8d25beafb506384d1b06e1a964cacb8ea
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-02 14:31:44 -0400

    Run first test.

---
+++ app/models/repo_call.rb
@@ -0,0 +1,12 @@
+class RepoCall
+
+  def intialize(username)
+    @username = username
+    @repo = get_data
+  end
+
+  def get_data
+
+  end
+
+end
---
+++ test/models/repo_call_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+require 'minitest/pride'
+
+
+require './app/models/repo_call.rb'
+
+class Repocall
+  def get_data
+    JSON.parse(File.open("./test/models/repo_call.json").read)
+  end
+end
+
+
+class RepoCallTest < MiniTest::Test
+
+  def test_create_call
+    assert true
+  end
+
+
+end
