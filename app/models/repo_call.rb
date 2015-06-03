cf6f010cb7409e72754b11f71f2ec2a683a3c123
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-02 20:27:04 -0400

    Pass tests for repocall.

--- app/models/repo_call.rb
+++ app/models/repo_call.rb
@@ -5,9 +5,32 @@ class RepoCall
     @repo = get_data
   end
 
-  def repo_name
-    #  @repo.each {|n| n["name"]}
-    @repo.map {|n| n["name"]}
+  def repo_names
+    @repo.map { |n| n["name"]}
+  end
+
+  def repo_urls
+    @repo.map { |m| m["html_url"]}
+  end
+
+  def repo_descriptions
+    @repo.map { |d| d["description"]}
+  end
+
+  def repo_update_dates
+    @repo.map { |u| u["updated_at"]}
+  end
+
+  def repo_languages
+    @repo.map { |l| l["language"]}
+  end
+
+  def stargazer_counts
+    @repo.map { |s| s["stargazers_count"]}
+  end
+
+  def fork_counts
+    @repo.map { |f| f["forks"]}
   end
 
   def get_data
--- test/models/repo_call_test.rb
+++ test/models/repo_call_test.rb
@@ -17,39 +17,39 @@ class RepoCallTest < MiniTest::Test
     assert RepoCall
   end
 
-  def test_returns_repo_name
+  def test_returns_repo_names
     call = RepoCall.new("masonfmatthews")
-    assert_equal "api_experiments", call.repo_name[0]
+    assert_equal "api_experiments", call.repo_names[0]
   end
 
-  def test_returns_repo_url
+  def test_returns_repo_urls
     call = RepoCall.new("masonfmatthews")
-    assert_equal "https://github.com/masonfmatthews/api_experiments", call.repo_url[0]
+    assert_equal "https://github.com/masonfmatthews/api_experiments", call.repo_urls[0]
   end
 
-  def test_returns_repo_description
+  def test_returns_repo_descriptions
     call = RepoCall.new("masonfmatthews")
-    assert_equal "Assignments for the Rails course at The Iron Yard's Durham Campus", call.repo_description[8]
+    assert_equal "Assignments for the Rails course at The Iron Yard's Durham Campus", call.repo_descriptions[8]
   end
 
   def test_returns_updated_at
     call = RepoCall.new("masonfmatthews")
-    assert_equal "2015-05-25T13:07:44Z", call.repo_updated_at[0]
+    assert_equal "2015-05-25T13:07:44Z", call.repo_update_dates[0]
   end
 
-  def test_returns_language
+  def test_returns_languages
     call = RepoCall.new("masonfmatthews")
-    assert_equal "Ruby", repo_language[0]
+    assert_equal "Ruby", call.repo_languages[0]
   end
 
-  def test_returns_stargazer_count
+  def test_returns_stargazer_counts
     call = RepoCall.new("masonfmatthews")
-    assert_equal 0, call.stargazer_count[0]
+    assert_equal 0, call.stargazer_counts[0]
   end
 
-  def test_returns_forks_count
+  def test_returns_forks_counts
     call = RepoCall.new("masonfmatthews")
-    assert_equal 0, call.fork_count[0]
+    assert_equal 0, call.fork_counts[0]
   end
 
 end
